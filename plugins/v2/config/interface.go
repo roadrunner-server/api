c66c7c0daa80194d02707ce9af7c0aef475ea1c2
Valery Piashchynski <piashchynski.valery@gmail.com>
2022-01-09 14:42:34 +0300

    update config's interface
    Signed-off-by: Valery Piashchynski <piashchynski.valery@gmail.com>

--- plugins/v2/config/interface.go
+++ plugins/v2/config/interface.go
@@ -30,16 +30,9 @@ type Configurer interface {
 	// Has checks if config section exists.
 	Has(name string) bool
 
-	// GetCommonConfig returns General section. Read-only
-	GetCommonConfig() *General
-}
+	// GracefulTimeout represents timeout for all servers registered in the endure
+	GracefulTimeout() time.Duration
 
-// General is the part of the config plugin which contains general for the whole RR2 parameters
-// For example - http timeouts, headers sizes etc and also graceful shutdown timeout should be the same across whole application
-type General struct {
-	// GracefulTimeout for the temporal and http
-	GracefulTimeout time.Duration
-
-	// RRVersion passed from the rr-binary
-	RRVersion *version.Version
+	// RRVersion returns running RR version
+	RRVersion() *version.Version
 }
