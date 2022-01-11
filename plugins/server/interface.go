271ee862c5ababf4d53f42a4b4a140af64271ba1
Valery Piashchynski <piashchynski.valery@gmail.com>
2022-01-12 00:29:40 +0300

    add pool's options to the Server interface
    Signed-off-by: Valery Piashchynski <piashchynski.valery@gmail.com>

--- plugins/server/interface.go
+++ plugins/server/interface.go
@@ -15,5 +15,5 @@ type Server interface {
 	// NewWorker return a new worker with provided and attached by the user listeners and environment variables
 	NewWorker(ctx context.Context, env map[string]string) (*worker.Process, error)
 	// NewWorkerPool return new pool of workers (PHP) with attached events listeners, env variables and based on the provided configuration
-	NewWorkerPool(ctx context.Context, opt *pool.Config, env map[string]string) (pool.Pool, error)
+	NewWorkerPool(ctx context.Context, opt *pool.Config, env map[string]string, options ...pool.Options) (pool.Pool, error)
 }
